87ccea113134c778a8896641e8ef90857b8bd1c4
Harry Maclean <harrymaclean@gocardless.com>
2016-12-14 16:28:49 +0000

    Add basic app with health check endpoint

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "github.com/kelseyhightower/envconfig"
+  "log"
+  "fmt"
+  "net/http"
+)
+
+type Config struct {
+  Port int `required:"true"`
+}
+
+func main() {
+  var c Config
+  err := envconfig.Process("draupnir", &c)
+  if err != nil {
+    log.Fatal(err.Error())
+  }
+
+  http.HandleFunc("/health_check", func (w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+    fmt.Fprintf(w, "OK")
+  })
+
+  err = http.ListenAndServe(fmt.Sprintf(":%d", c.Port), nil)
+  if err != nil {
+    log.Fatal(err.Error())
+  }
+}
