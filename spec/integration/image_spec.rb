1fa099647e8fba3e52634ecb437bfb929083d44e
Harry Maclean <harrymaclean@gocardless.com>
2017-02-28 23:46:30 +0000

    Update integration tests

--- spec/integration/image_spec.rb
+++ spec/integration/image_spec.rb
@@ -3,9 +3,13 @@ require 'spec_helper'
 
 RSpec.describe 'GET /images' do
   it "responds with 'OK'" do
-    response = RestClient.get('localhost:8080/images', accept: JSONAPI_CONTENT_TYPE)
+    response = RestClient.get(
+      'localhost:8080/images',
+      content_type: JSONAPI_CONTENT_TYPE
+    )
     expect(response.code).to eq(200)
-    expect(JSON.parse(response.body)).to be_a(Array)
+    json_body = JSON.parse(response.body)
+    expect(json_body["data"]).to be_a(Array)
   end
 end
 
@@ -14,17 +18,25 @@ RSpec.describe 'POST /images' do
     timestamp = Time.utc(2016, 1, 2, 3, 4, 5)
     response = RestClient.post(
       'localhost:8080/images',
-      { backed_up_at: timestamp.iso8601 }.to_json,
-      content_type: :json,
-      accept: JSONAPI_CONTENT_TYPE
+      {
+        data: {
+          type: 'images',
+          attributes: {
+            backed_up_at: timestamp.iso8601
+          }
+        }
+      }.to_json,
+      content_type: JSONAPI_CONTENT_TYPE
     )
 
     expect(response.code).to eq(201)
-    image = JSON.parse(response.body)
-    expect(image["id"]).to be_a(Numeric)
-    expect(Time.parse(image["backed_up_at"])).to eq(timestamp)
-    expect(image["ready"]).to eq(false)
-    expect(Time.parse(image["created_at"])).to be_a(Time)
-    expect(Time.parse(image["updated_at"])).to be_a(Time)
+    image = JSON.parse(response.body)["data"]
+    attrs = image["attributes"]
+    puts attrs
+    expect(image["id"]).to be_a(String)
+    expect(Time.parse(attrs["backed_up_at"])).to eq(timestamp)
+    expect(attrs["ready"]).to eq(false)
+    expect(Time.parse(attrs["created_at"])).to be_a(Time)
+    expect(Time.parse(attrs["updated_at"])).to be_a(Time)
   end
 end
